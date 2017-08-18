db47d710d4d7599e8f5dba41d238c0a2939c7f21
kjw <1186751305@qq.com>
2017-08-18 10:11:47 +0800

    vue
    vue-pro

---
+++ src/components/erjiyemian.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="gzh">
+    <h1>二级页面</h1>
+    <a> written by {{ author }} </a>
+  </div>
+</template>
+ 
+<script type="text/javascript">
+export default {
+  data () {
+    return {
+      author: "微信公众号 lion"
+    }
+  }
+}
+</script>
+ 
+<style>
+</style>
